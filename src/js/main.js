2970220b0173b9740d880c6c10a1da0dd1f6e48a
mharyst <mykhailo.haryst@gmail.com>
2016-08-17 02:12:48 +0300

    Added pop up, styles, functionality

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+//Showing Pop Up
+
+$(document).ready(function() {
+  $('.container #uninstall').click( function () {
+  	$('#overlay').fadeIn(400,
+  	 	function() {
+  			$('#pop_up')
+  				.css('display', 'flex')
+  				.animate({opacity: 1, top: '20%'}, 200);
+      });
+  });
+});
+
+//Close Pop Up
+function closePopUp() {
+$('#pop_up')
+  .animate({opacity: 0, top: '10%'}, 200,
+    function() {
+      $(this).css('display', 'none');
+      $('#overlay').fadeOut(50);
+    }
+  );
+}
+
+$('#pop_up button.cancel, #pop_up_close, #overlay').click(closePopUp);
+
+$('#pop_up button.confirm-uninstall').click( function() {
+  alert("Done!");
+  closePopUp();
+  // setTimeout(function(){
+  // }, 2000);
+});
